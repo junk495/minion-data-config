20351b4acb2b8f0751deaecb5b51a2666569452f
pi <emefge@gmx.at>
2022-12-05 15:16:22 +0100

    input-shaper

--- config/scripts/generate-shaper-graph-x.sh
+++ config/scripts/generate-shaper-graph-x.sh
@@ -2,10 +2,10 @@
 
 NEWX=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
 DATE=$(date +'%Y-%m-%d-%H%M%S')
-if [ ! -d "/home/pi/klipper_config/input_shaper" ]
+if [ ! -d "/home/pi/Minion-data/input_shaper" ]
 then
     mkdir /home/pi/klipper_config/input_shaper
-    chown pi:pi /home/pi/klipper_config/input_shaper
+    chown pi:pi /home/pi/Minion-data/input_shaper
 fi
 
-~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_x_$DATE.png
+~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/Minion-data/input_shaper/resonances_x_$DATE.png
